4ed2414a2a90f904f665ac04f4f593b43d920796
Nazar Dmytrenko <nazarkadm@gmail.com>
2023-09-07 18:02:15 +0300

    Add DyshType model to models.py

--- restaurant_service/models.py
+++ restaurant_service/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Dish Type"
+        verbose_name_plural = "Dish Types"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+
