5c61a0bb5aaff3177e9b0f334991284986ef8e1f
Nazar Dmytrenko <nazarkadm@gmail.com>
2023-09-07 18:06:32 +0300

    Add Cook model to models.py

--- restaurant_service/models.py
+++ restaurant_service/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -12,5 +13,14 @@ class DishType(models.Model):
     def __str__(self) -> str:
         return self.name
 
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["username"]
+
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name} {self.last_name})"
 
 
