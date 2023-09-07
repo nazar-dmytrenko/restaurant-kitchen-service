15f7bd9f67fa1686b1217cb1753c2e5304824c32
Nazar Dmytrenko <nazarkadm@gmail.com>
2023-09-07 18:22:15 +0300

    Specify the 'AUTH_USER_MODEL' and add 'taresaurant_service'o installed_apps in setâtings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "restaurant_service"
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "restaurant_service.Cook"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
