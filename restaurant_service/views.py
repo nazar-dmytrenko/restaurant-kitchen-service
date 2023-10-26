55c97bbca568cad5169a248ab7122648e00bbf0f
Nazar Dmytrenko <nazarkadm@gmail.com>
2023-10-26 19:25:53 +0300

    Add imports in view.py

--- restaurant_service/views.py
+++ restaurant_service/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, login, authenticate
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import View, generic
 
-# Create your views here.
+
+from restaurant_service.forms import (
+    CookCreationForm,
+    CookSearchForm,
+    CookUpdateForm,
+    DishForm,
+    DishSearchForm,
+    DishTypeForm,
+    DishTypeSearchForm
+)
+from restaurant_service.models import (Cook, Dish, DishType)
