4106d2f42eb74b2841d55ba406e9e330b24d8fed
Nazar Dmytrenko <nazarkadm@gmail.com>
2023-10-30 18:27:07 +0200

    Add Dish views and IndexView in views.py

--- restaurant_service/views.py
+++ restaurant_service/views.py
@@ -16,3 +16,58 @@ from restaurant_service.forms import (
     DishTypeSearchForm
 )
 from restaurant_service.models import (Cook, Dish, DishType)
+
+
+class IndexView(View):
+    @staticmethod
+    def get(request):
+        return render(request, "")  #restaurant/index.html
+
+
+class DishListView(generic.ListView):
+    model = Dish
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs) -> dict:
+        context = super(DishListView, self).get_context_data(**kwargs)
+
+        name = self.request.GET.get("name", "")
+
+        context["search_form"] = DishSearchForm(initial={
+            "name": name
+        })
+
+        return context
+
+    def get_queryset(self):
+        queryset = Dish.objects.select_related("dish_type")
+        form = DishSearchForm(self.request.GET)
+
+        if form.is_valid():
+            return queryset.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+        return queryset
+
+
+class DishDetailView(generic.DetailView):
+    model = Dish
+    queryset = Dish.objects.prefetch_related("cooks")
+
+
+class DishCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Dish
+    form_class = DishForm
+    template_name = "" #restaurant/dish_form.html
+
+
+class DishUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Dish
+    form_class = DishForm
+    template_name = "" #restaurant/dish_form.html
+
+
+class DishDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Dish
+    success_url = reverse_lazy("restaurant:dish-list")
+    template_name = "" #restaurant/dish_confirm_delete.html
