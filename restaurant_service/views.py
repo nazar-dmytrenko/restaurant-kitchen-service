f46c2114efee4e9fc8ef00ff484760959d974db0
Nazar Dmytrenko <nazarkadm@gmail.com>
2023-11-04 13:59:27 +0200

    Add DishType views in views.py

--- restaurant_service/views.py
+++ restaurant_service/views.py
@@ -71,3 +71,60 @@ class DishDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Dish
     success_url = reverse_lazy("restaurant:dish-list")
     template_name = "" #restaurant/dish_confirm_delete.html
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    context_object_name = "dish_type_list"
+    template_name = "restaurant/dish_type_list.html"
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs) -> dict:
+        context = super(DishTypeListView, self).get_context_data(**kwargs)
+
+        name = self.request.GET.get("name", "")
+
+        context["search_form"] = DishTypeSearchForm(initial={
+            "name": name
+        })
+
+        return context
+
+    def get_queryset(self):
+        queryset = DishType.objects.all()
+        form = DishTypeSearchForm(self.request.GET)
+
+        if form.is_valid():
+            return queryset.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+        return queryset
+
+
+class DishTypeDetailView(generic.DetailView):
+    template_name = "" #restaurant/dish_type_detail.html
+    context_object_name = "dish_type"
+    model = DishType
+    queryset = DishType.objects.all()
+
+
+class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
+    model = DishType
+    form_class = DishTypeForm
+    context_object_name = "dish_type"
+    template_name = "" #restaurant/dish_type_form.html
+
+
+class DishTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = DishType
+    context_object_name = "dish_type"
+    form_class = DishTypeForm
+    template_name = "" #restaurant/dish_type_form.html
+
+
+class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = DishType
+    context_object_name = "dish_type"
+    success_url = reverse_lazy("restaurant:dish-type-list")
+    template_name = "" #restaurant/dish_type_confirm_delete.html
+
