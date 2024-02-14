772c53f6117e5200d1edbe454a06a1810ec2ce0b
Nazar Dmytrenko <nazarkadm@gmail.com>
2024-02-15 00:29:14 +0200

    Add pagination on dish list page

--- restaurant_service/views.py
+++ restaurant_service/views.py
@@ -85,6 +85,7 @@ def index_view(request):
 
 class DishListView(generic.ListView):
     model = Dish
+    paginate_by = 5
     template_name = "restaurant/dish_list.html"
 
     def get_context_data(self, *, object_list=None, **kwargs) -> dict:
