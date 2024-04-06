51021296a71e5fba1d3a1c5d1ac161083dd4e5a8
Nazar Dmytrenko <nazarkadm@gmail.com>
2024-04-06 13:02:46 +0300

    update settings and requirements.txt

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-qsa(yer0p@u1sgerp9*pw&%-^(@+2@mr+3hxusyog%httnkmm0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
